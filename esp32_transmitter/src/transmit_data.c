f184e906abf118099ef5b6a11ac34b9c2631fab8
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-16 15:14:12 +0800

    sending data packets between esp32s

---
+++ esp32_transmitter/src/transmit_data.c
@@ -0,0 +1,3 @@
+// accept an input data from a button click, but that button is pressed on the
+// esp32 send it via ESP-NOW, the esp32 receives the data, it will then send it
+// to the LED to blink it.
