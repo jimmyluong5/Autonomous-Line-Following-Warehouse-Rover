a5685ae5679ece34ea72153ccd61f6e1d508f273
jimmyluong5 <jimmyluong@yahoo.com>
2026-09-02 23:24:11 +0800

    working on the lcd file

---
+++ esp32_transmitter/src/lcd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+
+#include "pretty_effect.h"
+
+
