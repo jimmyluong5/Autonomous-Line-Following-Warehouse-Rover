b565098f9ceb332024f0914d42b7d92d1c219ae7
jimmyluong5 <jimmyluong@yahoo.com>
2026-09-10 14:22:03 +0800

    adding failsafe in case we don't send any packets to the robot

--- esp32_transmitter/include/speaker.h
+++ esp32_transmitter/include/speaker.h
@@ -8,5 +8,5 @@ void init_speaker(void);
 void speaker_update(uint8_t button_packet);
 void speaker_on(void);
 void speaker_off(void);
-
+void speaker_pattern(int beep_count, uint32_t beep_on, uint32_t beep_off);
 #endif
